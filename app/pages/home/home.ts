65787d6b60885af718e80c3efbda459895dafbb4
Jessamyn Smith <geekchick77@gmail.com>
2016-09-27 00:06:51 -0400

    Added initial brand data

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,18 +1,21 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {BrandData} from '../../providers/brands';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers: [BrandData]
 })
 export class HomePage {
-  brand:any;
-  owner:any;
+  brandName: any;
+  brandInfo: any;
+  brandData: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, brandData: BrandData) {
+    this.brandData = brandData;
   }
 
   checkBrand() {
-    this.owner = this.brand;
+    this.brandInfo = this.brandData.getOwner(this.brandName);
   }
 }
