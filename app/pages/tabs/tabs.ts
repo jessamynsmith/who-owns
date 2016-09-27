bcc7018ff4d532fe50078491c7c56e4691e77be9
Jessamyn Smith <geekchick77@gmail.com>
2016-09-26 22:09:40 -0400

    Made home page, removed unused pages.

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import {Component} from '@angular/core';
+import {HomePage} from '../home/home';
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -9,14 +7,10 @@ import { ContactPage } from '../contact/contact';
 export class TabsPage {
 
   public tab1Root: any;
-  public tab2Root: any;
-  public tab3Root: any;
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
   }
 }
