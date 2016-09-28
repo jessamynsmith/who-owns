9c93324221b8c92e80fdac5b7776a5d75881e6b3
Jessamyn Smith <geekchick77@gmail.com>
2016-09-27 20:59:24 -0400

    Added autocompletion

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,18 +1,28 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {BrandData} from '../../providers/brands';
+import {CompleterCmp, CompleterService, CompleterData, COMPLETER_DATA_PROVIDERS} from 'ng2-completer';
+
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
-  providers: [BrandData]
+  directives: [CompleterCmp],
+  providers: [COMPLETER_DATA_PROVIDERS, CompleterService, BrandData]
 })
 export class HomePage {
   brandName: any;
   brandInfo: any;
   brandData: any;
+  private dataService: CompleterData;
 
-  constructor(public navCtrl: NavController, brandData: BrandData) {
+  constructor(public navCtrl: NavController, private completerService: CompleterService,
+              brandData: BrandData) {
     this.brandData = brandData;
+    this.dataService = completerService.local(this.brandData.data, 'name', 'name');
+  }
+
+  clearBrand() {
+    this.brandName = '';
   }
 
   checkBrand() {
